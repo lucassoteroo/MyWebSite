45481715df3d543b7d5da2728ad2d6e111bb9d75
lucassoteroo <lucassoteroo14@gmail.com>
2021-11-26 22:59:59 -0300

    tentando criar o dark Mode

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+const darkMode = document.getElementById("darkMode");
+const elementBody = document.getElementById("body");
+const elementLabel = document.getElementById("label");
+
+function marcado() {
+    if (true) {
+        elementBody.addClass('darkMode')
+    } else if (false) {
+        return
+    }
+}
